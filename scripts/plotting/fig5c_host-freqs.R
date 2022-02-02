2212b405a856494de279ef2ebecc6829c9021b38
alexmsalmeida <alexandre.m.s.almeida@gmail.com>
2022-02-02 23:48:57 +0000

    Update scripts

---
+++ scripts/plotting/fig5c_host-freqs.R
@@ -0,0 +1,32 @@
+# load libraries
+library(RColorBrewer)
+library(ggplot2)
+library(tidyr)
+
+# load data
+setwd("~/Documents/ESPOD/Analyses/Project_latinbiota")
+host.bacteria = read.delim("viruses/virus_host_all.tsv", header=TRUE, stringsAsFactors = FALSE)
+host.bacteria.tax.filt = read.delim("viruses/virus_host_summary.tsv", header=TRUE, stringsAsFactors = FALSE)
+
+# parse taxonomy
+keep.taxa = sort(names(sort(table(host.bacteria.tax.filt$Family), decreasing=TRUE)[1:10]))
+host.bacteria.tax.filt$Family = ifelse(host.bacteria.tax.filt$Family %in% keep.taxa, host.bacteria.tax.filt$Family, "Other")
+order.taxa = c(names(sort(table(host.bacteria.tax.filt$Family[which(host.bacteria.tax.filt$Family != "Other")]), decreasing=TRUE)), "Other")
+host.bacteria.tax.filt$Family = factor(host.bacteria.tax.filt$Family, levels=order.taxa)
+taxa.colors = c(brewer.pal(10, "Set3"), "darkgrey")
+names(taxa.colors) = order.taxa
+
+# plot family counts
+df = data.frame(table(host.bacteria.tax.filt$Family))
+colnames(df) = c("Family", "Counts")
+host.plot = ggplot(df, aes(x=Family, y=Counts, fill=Family)) +
+  geom_bar(stat="identity", colour="darkgrey", alpha=0.8) +
+  theme_classic() +
+  coord_flip() +
+  ylab("Number of viral species") +
+  scale_fill_manual(values=taxa.colors) +
+  theme(axis.text.x = element_text(size=12)) +
+  theme(axis.text.y = element_text(size=12)) +
+  theme(axis.title.x = element_text(size=14)) +
+  theme(axis.title.y = element_blank())
+ggsave("../../../Publications/2022-Latinbiota/Figures/figure5/host_counts.pdf", width=8, height=5)
