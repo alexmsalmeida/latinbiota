1b98860dd34b4d968f27e01878ee055ecda363e7
alexmsalmeida <alexandre.m.s.almeida@gmail.com>
2022-02-03 00:08:58 +0000

    Update scripts

--- scripts/plotting/fig5c_host-freqs.R
+++ scripts/plotting/fig5c_host-freqs.R
@@ -5,8 +5,7 @@ library(tidyr)
 
 # load data
 setwd("~/Documents/ESPOD/Analyses/Project_latinbiota")
-host.bacteria = read.delim("viruses/virus_host_all.tsv", header=TRUE, stringsAsFactors = FALSE)
-host.bacteria.tax.filt = read.delim("viruses/virus_host_summary.tsv", header=TRUE, stringsAsFactors = FALSE)
+host.bacteria.tax.filt = read.delim("viruses/vcontact/virus_host_summary.tsv", header=TRUE, stringsAsFactors = FALSE)
 
 # parse taxonomy
 keep.taxa = sort(names(sort(table(host.bacteria.tax.filt$Family), decreasing=TRUE)[1:10]))
@@ -20,13 +19,14 @@ names(taxa.colors) = order.taxa
 df = data.frame(table(host.bacteria.tax.filt$Family))
 colnames(df) = c("Family", "Counts")
 host.plot = ggplot(df, aes(x=Family, y=Counts, fill=Family)) +
-  geom_bar(stat="identity", colour="darkgrey", alpha=0.8) +
+  geom_bar(stat="identity", colour="darkgrey", alpha=0.8, size=0.3) +
   theme_classic() +
   coord_flip() +
+  guides(fill="none") +
   ylab("Number of viral species") +
   scale_fill_manual(values=taxa.colors) +
   theme(axis.text.x = element_text(size=12)) +
   theme(axis.text.y = element_text(size=12)) +
   theme(axis.title.x = element_text(size=14)) +
   theme(axis.title.y = element_blank())
-ggsave("../../../Publications/2022-Latinbiota/Figures/figure5/host_counts.pdf", width=8, height=5)
+ggsave("../../../Publications/2022-Latinbiota/Figures/figure5/host_counts.pdf", width=7, height=5)
